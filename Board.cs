1a1e533c045a2ee3b6bbb65280bd693f9d2cd98a
DanielLopezWin14 <daniel.lopez.win14@utb.hermods.se>
2014-10-30 12:05:50 +0100

    The gameboard
    2 classes from where we can adjust the board itself.

---
+++ Board.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessgame
+{
+    public class Board : Shape
+    {
+        public int Length;
+        public Board(Position StartPos, int Length)
+        {
+            StartPosition = StartPos;
+            this.Length = Length;
+        }
+        public override void Draw()
+        {
+            int startX = StartPosition.X;
+            int startY = StartPosition.Y;
+            
+            for (int y = startY; y < startY + Length; y++)
+            {
+                Console.SetCursorPosition(startX, y);
+                Console.ForegroundColor = ConsoleColor.White;
+                
+                for (int x = startX; x < startX + Length; x++)
+                {
+                    
+                    Console.Write("[]");
+                    
+                }
+                Console.Write("[]");
+            }
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chessgame
+{
+    public abstract class Shape
+    {
+        protected Position StartPosition;
+
+        public virtual void Draw()
+        {
+        }
+    }
+}
