b2dd10fdaced24a1d0b47a9513be068f665ea3c9
DanielLopezWin14 <daniel.lopez.win14@utb.hermods.se>
2014-10-30 13:21:10 +0100

    some changes in proram to start the game
    Just to get some kind of alternatives if you want to play or not

--- Program.cs
+++ Program.cs
@@ -10,7 +10,28 @@ namespace chessgame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("              This is Chess\r\n (created by Roje, Tobias and Daniél)");
+            Console.WriteLine(" Want to try it out? \r\n \r\n [Y]es / [N]o");
+            string line = Console.ReadLine();
+            
+            while (true) 
+            {
+                Console.Clear();
+                if (line == "Y" || line == "y") 
+                {
+                    Board GB = new Board(new Position(5, 5), 8);
 
+                    List<Shape> SL = new List<Shape>();
+                    SL.Add(GB);
+                       
+                    GB.Draw();
+                 }
+                else
+                {
+                    break;
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
