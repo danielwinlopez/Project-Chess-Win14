81446ca946af96238aac037fbf72b8b7b3a060a1
roje <roje.haddad.win14@utb.hermods.se>
2014-11-06 14:29:26 +0100

    moves queen

--- Move and Position/Move.cs
+++ Move and Position/Move.cs
@@ -8,20 +8,38 @@ namespace chessgame
 {
     public class Move
     {
-        //public List<Position> PawnMoveList()
-        //{
-        //    List<Position> oldPosition = new List<Position>();
-        //    List<Position> newPosition = oldPosition;
+        public Position GetMove { get; set; }
 
-        //    foreach (var item in oldPosition)
-        //    {
-        //        for (int y = 0; y < 2; y++)
-        //        {
-        //            newPosition.Add(new Position(X, Y + y));
-        //            oldPosition.Clear();
-        //        }
-        //    }
-        //    return newPosition;
-        //}
+
+        public List<Position> QueenMove()
+        {
+            List<Position>moveList = new List<Position>();
+
+            moveList.Add(new Position(1, 1));
+            moveList.Add(new Position(-1, -1));
+            moveList.Add(new Position(0, 1));
+            moveList.Add(new Position(1, 0));
+            moveList.Add(new Position(0, -1));
+            moveList.Add(new Position(-1, 0));
+            moveList.Add(new Position(-1, 1));
+            moveList.Add(new Position(1,-1));
+
+            int antalSteg = 7;
+            foreach (var item in moveList)
+            {
+               List<Position>newList = new List<Position>();
+                
+                for (int i = 0; i < antalSteg; i++)
+                {
+                    var stegX = item.X + GetMove.X;
+                    var stegY = item.Y + GetMove.Y;
+                    newList.Add(new Position(stegX,stegY));
+                }
+
+            }
+            return moveList;
+
+
+        }
     }
 }
